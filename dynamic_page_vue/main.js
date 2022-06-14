a1901e5c3d1f3d09ff3c0183534f8f643ec2e77b
jpruful <76832407+jpruful@users.noreply.github.com>
2022-06-15 05:50:18 +0800

    Add files via upload

---
+++ dynamic_page_vue/main.js
@@ -0,0 +1,23 @@
+let pageData = {
+    members : [
+        {
+            name:'Susan',
+            gender:'Female'
+        },
+        {
+            name:'Peter',
+            gender:'Male'
+        },
+        {
+            name:'Bill',
+            gender:'Unknown'
+        }]
+};
+
+const app = Vue.createApp({
+    data(){
+        return pageData;
+    }
+});
+
+app.mount("#app");
